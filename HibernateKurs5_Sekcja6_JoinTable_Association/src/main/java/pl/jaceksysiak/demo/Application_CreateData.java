178a37904497ecb3249c70df571b9093256965c8
Jacek <Jacek@BERLIN>
2020-04-07 05:34:30 +0200

    c

--- HibernateKurs5_Sekcja6_JoinTable_Association/src/main/java/pl/jaceksysiak/demo/Application_CreateData.java
+++ HibernateKurs5_Sekcja6_JoinTable_Association/src/main/java/pl/jaceksysiak/demo/Application_CreateData.java
@@ -8,6 +8,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import pl.jaceksysiak.demo.entity.Account;
+import pl.jaceksysiak.demo.entity.Budget;
 import pl.jaceksysiak.demo.entity.Transaction;
 
 
@@ -18,8 +19,9 @@ public class Application_CreateData {
 		// create session factory
 		SessionFactory factory = new Configuration()
 								.configure("hibernate.cfg.xml")
-								.addAnnotatedClass(Account.class)
+								.addAnnotatedClass(Budget.class)
 								.addAnnotatedClass(Transaction.class)
+								.addAnnotatedClass(Account.class)
 								.buildSessionFactory();
 		
 		// create session
@@ -30,17 +32,9 @@ public class Application_CreateData {
 			session.beginTransaction();
 			
 			//Creating the Object 
-			Account account = createNewAccount();
-			
-			account.getTransactions().add(createNewBeltPurchase(account));
-			account.getTransactions().add(createShoePurchase(account));
-			
+		
 			//Saving the Object to DB
-			session.save(account);
-			
-			Transaction dbTransaction = (Transaction)session.get(Transaction.class, account.getTransactions().get(0).getTransactionId());
-			System.out.println(dbTransaction.getAccount().getName());
-						
+			 	
 			// commit transaction
 			session.getTransaction().commit();
 		}
@@ -51,50 +45,4 @@ public class Application_CreateData {
 			factory.close();
 		}
 	}
-	
-	private static Transaction createNewBeltPurchase(Account account) {
-		Transaction beltPurchase = new Transaction();
-		beltPurchase.setAccount(account);
-		beltPurchase.setTitle("Dress Belt");
-		beltPurchase.setAmount(new BigDecimal("50.00"));
-		beltPurchase.setClosingBalance(new BigDecimal("0.00"));
-		beltPurchase.setCreatedBy("Kevin Bowersox");
-		beltPurchase.setCreatedDate(new Date());
-		beltPurchase.setInitialBalance(new BigDecimal("0.00"));
-		beltPurchase.setLastUpdatedBy("Kevin Bowersox");
-		beltPurchase.setLastUpdatedDate(new Date());
-		beltPurchase.setNotes("New Dress Belt");
-		beltPurchase.setTransactionType("Debit");
-		return beltPurchase;
 	}
-
-	private static Transaction createShoePurchase(Account account) {
-		Transaction shoePurchase = new Transaction();
-		shoePurchase.setAccount(account);
-		shoePurchase.setTitle("Work Shoes");
-		shoePurchase.setAmount(new BigDecimal("100.00"));
-		shoePurchase.setClosingBalance(new BigDecimal("0.00"));
-		shoePurchase.setCreatedBy("Kevin Bowersox");
-		shoePurchase.setCreatedDate(new Date());
-		shoePurchase.setInitialBalance(new BigDecimal("0.00"));
-		shoePurchase.setLastUpdatedBy("Kevin Bowersox");
-		shoePurchase.setLastUpdatedDate(new Date());
-		shoePurchase.setNotes("Nice Pair of Shoes");
-		shoePurchase.setTransactionType("Debit");
-		return shoePurchase;
-	}
-
-	private static Account createNewAccount() {
-		Account account = new Account();
-		account.setCloseDate(new Date());
-		account.setOpenDate(new Date());
-		account.setCreatedBy("Kevin Bowersox");
-		account.setInitialBalance(new BigDecimal("50.00"));
-		account.setName("Savings Account");
-		account.setCurrentBalance(new BigDecimal("100.00"));
-		account.setLastUpdatedBy("Kevin Bowersox");
-		account.setLastUpdatedDate(new Date());
-		account.setCreatedDate(new Date());
-		return account;
-	}
-}
